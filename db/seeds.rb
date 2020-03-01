400ea1b2ec703dfbb9baad56c7eb64b3a1b09138
Adebola <onedebos@gmail.com>
2020-03-01 17:57:20 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Account.create!(
+    name: 'test',
+    bank: 'test',
+    acc_no:'309039309',
+    category:'yes'
+    user_id:1
+)
+
+User.create!(
+    id:1,
+    username:'admin',
+    password: 'password'
+)
